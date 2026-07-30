2043eec8d878e58bac2b12d3994a3c11c9ea844e
Manas Dixit <manasdixit4100@gmail.com>
2026-07-30 10:36:18 +0530

    Point frontend domain to sathee.vercel.app.

--- Server/src/index.js
+++ Server/src/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://hcl-sathee.vercel.app",
+      "https://sathee.vercel.app",
       /\.vercel\.app$/,
       "http://localhost:5173",
       "http://127.0.0.1:5173",
